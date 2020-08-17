d0eb4d9b02d9beacdae0370a48e29bcee25abace
KhalilMay <khello1996@gmail.com>
2020-08-17 15:55:29 +0200

    Changes to start a thread to run the server in the load function and stop the server when the experiment is stopped in the unload function

--- AndroidRunner/Plugins/perfume_js/Perfume_js.py
+++ AndroidRunner/Plugins/perfume_js/Perfume_js.py
@@ -8,6 +8,7 @@ from AndroidRunner.Plugins.perfume_js.server import start_server, stop_server
 
 from AndroidRunner.Plugins.Profiler import Profiler
 
+daemon = ""
 
 class Perfume_js(Profiler):
     def __init__(self, config, paths):
@@ -16,18 +17,21 @@ class Perfume_js(Profiler):
         self.logcat_output = ''
         self.profile = False
         self.metrics = config['metrics']
+        #self.httpd= ""
 
     def start_profiling(self, device, **kwargs):
         self.profile = True
         self.logcat_output = '{}logcat_{}_{}.txt'.format(self.output_dir, device.id, time.strftime('%Y.%m.%d_%H%M%S'))
-        daemon = threading.Thread(name='daemon_server', target=start_server)	
-        daemon.setDaemon(True) # Set as a daemon so it will be killed once the main thread is dead.
-        daemon.start()
+        #global daemon
+
+        os.mkdir('output')#Adds an empty 'output' folder so if the internet connection 
+
         time.sleep(1)
 
     def stop_profiling(self, device, **kwargs):
         self.profile = False
-        stop_server()
+        #global daemon
+
 
     def collect_results(self, device, path=None):
         if(os.path.isdir("output")):#check if the folder is created, sometimes if the internet connection is done server.py won't create the folder
@@ -37,6 +41,7 @@ class Perfume_js(Profiler):
                 if any(metr in onefile for metr in self.metrics):
                     newFilesDestination= shutil.move("output/"+onefile,self.output_dir)
             shutil.rmtree("output/")
+
     def set_output(self, output_dir):
         self.output_dir = output_dir
 
@@ -46,9 +51,14 @@ class Perfume_js(Profiler):
     def load(self, device):
         if(os.path.isdir("output")): #deletes folder called 'output' in the root directory (NOTE: The root directory here is the directory where the experiment in executed)
             shutil.rmtree("output/")
+
+        daemon = threading.Thread(name='daemon_server', target=start_server)
+        daemon.setDaemon(True) # Set as a daemon so it will be killed once the main thread is dead.
+        daemon.start()
         return
 
     def unload(self, device):
+        stop_server()
         return
 
     def aggregate_subject(self):
